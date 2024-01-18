db545d135988c82afb94475499195dd1e5148d07
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-01-18 15:47:47 -0300

    Lesson 5 - Extracting a nested Livewire component

--- app/Livewire/ShowPosts.php
+++ app/Livewire/ShowPosts.php
@@ -2,23 +2,11 @@
 
 namespace App\Livewire;
 
-use App\Livewire\Forms\CreatePost;
 use App\Models\Post;
 use Livewire\Component;
 
 class ShowPosts extends Component
 {
-    public CreatePost $form;
-
-    public $showAddPostDialog = false;
-
-    public function add()
-    {
-        $this->form->save();
-
-        $this->reset('showAddPostDialog');
-    }
-
     public function delete($postId)
     {
         $post = Post::find($postId);
