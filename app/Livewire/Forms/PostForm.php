ddffafc1466a06e20bbd91792a7c9b658cd78250
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-01-19 10:29:39 -0300

    Lesson 7 - Adding an edit modal

--- app/Livewire/Forms/PostForm.php
+++ app/Livewire/Forms/PostForm.php
@@ -4,16 +4,25 @@ namespace App\Livewire\Forms;
 
 use App\Models\Post;
 use Livewire\Attributes\Rule;
-use Livewire\Attributes\Validate;
 use Livewire\Form;
 
-class CreatePost extends Form
+class PostForm extends Form
 {
     #[Rule('required|min:3')]
     public $title = '';
 
     #[Rule('required')]
     public $content = '';
+
+    public Post $post;
+
+    public function setPost($post)
+    {
+        $this->post = $post;
+
+        $this->title = $post->title;
+        $this->content = $post->content;
+    }
 
     public function save()
     {
@@ -25,5 +34,15 @@ class CreatePost extends Form
         ]);
 
         $this->reset(['title', 'content']);
+    }
+
+    public function update()
+    {
+        $this->validate();
+
+        $this->post->update([
+            'title' => $this->title,
+            'content' => $this->content,
+        ]);
     }
 }
--- app/Livewire/PostRow.php
+++ app/Livewire/PostRow.php
@@ -2,12 +2,31 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Forms\PostForm;
 use App\Models\Post;
 use Livewire\Component;
 
 class PostRow extends Component
 {
-    public Post $post;
+    public $post;
+
+    public PostForm $form;
+
+    public $showEditDialog = false;
+
+    public function mount()
+    {
+        $this->form->setPost($this->post);
+    }
+
+    public function save()
+    {
+        $this->form->update();
+
+        $this->post->refresh();
+
+        $this->reset('showEditDialog');
+    }
 
     public function render()
     {
