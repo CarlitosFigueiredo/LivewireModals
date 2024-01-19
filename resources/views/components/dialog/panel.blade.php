50926f0092c37f35072779cb8ee57df661627615
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-01-19 11:53:17 -0300

    Lesson 11 - Adding "shipe to close"

--- resources/views/components/dialog/panel.blade.php
+++ resources/views/components/dialog/panel.blade.php
@@ -3,7 +3,7 @@
         x-dialog
         x-model="dialogOpen"
         style="display: none"
-        class="fixed inset-0 overflow-y-auto z-10 text-left"
+        class="fixed inset-0 overflow-y-auto z-10 text-left pt-[30%] sm:pt-0"
     >
         <!-- Overlay -->
         <div x-dialog:overlay x-transition:enter.opacity class="fixed inset-0 bg-black/25"></div>
@@ -15,6 +15,20 @@
                 x-transition.in
                 class="relative max-w-xl w-full bg-white rounded-t-xl sm:rounded-b-xl shadow-lg overflow-hidden"
             >
+                <!-- Mobile: Top "grab" handle... -->
+                <div
+                    class="sm:hidden absolute top-[-10px] left-0 right-0 h-[50px]"
+                    x-data="{ startY: 0, currentY: 0, moving: false, get distance() { return this.moving ? Math.max(0, this.currentY - this.startY): 0 } }"
+                    x-on:touchstart="moving = true; startY = currentY = $event.touches[0].clientY"
+                    x-on:touchmove="currentY = $event.touches[0].clientY"
+                    x-on:touchend="if (distance > 100) $dialog.close(); moving = false;"
+                    x-effect="$el.parentElement.style.transform = 'translateY('+distance+'px)'"
+                >
+                    <div class="flex justify-center pt-[12px]">
+                        <div class="bg-gray-400 rounded-full w-[10%] h-[5px]"></div>
+                    </div>
+                </div>
+
                 <!-- Close Button -->
                 <div class="absolute top-0 right-0 pt-4 pr-4">
                     <button type="button" x-on:click="$dialog.close()" class="bg-gray-50 rounded-lg p-2 text-gray-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
